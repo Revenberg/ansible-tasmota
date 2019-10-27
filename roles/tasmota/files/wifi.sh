84c17a6ed0951eaada99099d475c03e3df611a27
Your Name <you@example.com>
2019-10-27 18:59:15 +0100

    .

--- roles/tasmota/files/wifi.sh
+++ roles/tasmota/files/wifi.sh
@@ -9,12 +9,11 @@ while [ 1 ]; do
     echo $ESSID
     if [ -f "~/ESSID_$ESSID.txt" ]; then
       sudo nmcli device wifi con "$ESSID" password "12345678"
+      "python3 /home/pi/SonOTA-master/SonOTA.py --serving-host $1 --wifi-ssid $2 --wifi-password $3 1>~/SonOTA.log"
       rm ~/ESSID_*.txt  2>/dev/null
       date > ~/ESSID_$ESSID.txt
     fi
   fi
-
-
 
   ESSID=$(iwlist wlan0 scan | grep FinalStage | cut -d'"' -f 2)
   if [ "$ESSID" == "FinalStage" ]; then
