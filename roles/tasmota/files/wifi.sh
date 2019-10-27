34a0910ac852055be2c11436cccdf5a7034a3759
Your Name <you@example.com>
2019-10-27 19:25:06 +0100

    .

--- roles/tasmota/files/wifi.sh
+++ roles/tasmota/files/wifi.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
 rm ~/ESSID_*.txt 2>/dev/null
 echo "Pres CTRL+C to stop..."
 while [ 1 ]; do
@@ -9,10 +11,10 @@ while [ 1 ]; do
 
   ESSID=$(iwlist wlan0 scan | grep ITEAD | cut -d'"' -f 2)
   if [ "$ESSID" != "" ]; then
-    echo $ESSID >> $4
+    echo $ESSID >> $3
     if [ -f "~/ESSID_$ESSID.txt" ]; then
-      sudo nmcli device wifi con "$ESSID" password "12345678" >> $4
-      python3 /home/pi/SonOTA-master/SonOTA.py --serving-host $1 --wifi-ssid $2 --wifi-password $3 >> $4
+      sudo nmcli device wifi con "$ESSID" password "12345678" >> $3
+      python3 /home/pi/SonOTA-master/SonOTA.py --serving-host $ip --wifi-ssid $1 --wifi-password $2 >> $3
       rm ~/ESSID_*.txt  2>/dev/null
       date > ~/ESSID_$ESSID.txt
     fi
@@ -20,14 +22,14 @@ while [ 1 ]; do
 
   ESSID=$(iwlist wlan0 scan | grep FinalStage | cut -d'"' -f 2)
   if [ "$ESSID" == "FinalStage" ]; then
-    echo $ESSID >> $4
+    echo $ESSID >> $3
     if [ -f "~/ESSID_FinalStage.txt" ]; then
-      sudo nmcli device wifi con "FinalStage" >> $4
+      sudo nmcli device wifi con "FinalStage" >> $3
       rm ~/ESSID_*.txt 2>/dev/null
       exit 0
     fi
   fi
   sleep 1
 
-  date >> $4
+  date >> $3
 done
