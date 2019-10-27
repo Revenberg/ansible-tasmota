3aeb27e27082fc08ea0cd7e8168497e33369caf3
Your Name <you@example.com>
2019-10-27 17:31:41 +0100

    .

---
+++ roles/tasmota/files/wifi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+rm ~/ESSID_*.txt 2>/dev/null
+echo "Pres CTRL+C to stop..."
+while [ 1 ]; do
+  iwlist wlan0 scan | grep ESSID | cut -d'"' -f 2
+
+  ESSID=$(iwlist wlan0 scan | grep ITEAD | cut -d'"' -f 2)
+  if [ "$ESSID" != "" ]; then
+    echo $ESSID
+    if [ -f "~/ESSID_$ESSID.txt" ]; then
+      sudo nmcli device wifi con "$ESSID" password "12345678"
+      rm ~/ESSID_*.txt  2>/dev/null
+      date > ~/ESSID_$ESSID.txt
+    fi
+  fi
+
+
+
+  ESSID=$(iwlist wlan0 scan | grep FinalStage | cut -d'"' -f 2)
+  if [ "$ESSID" == "FinalStage" ]; then
+    echo $ESSID
+    if [ -f "~/ESSID_FinalStage.txt" ]; then
+      sudo nmcli device wifi con "FinalStage"
+      rm ~/ESSID_*.txt 2>/dev/null
+      date > ~/ESSID_FinalStage.txt
+    fi
+  fi
+  sleep 1
+
+  date
+done
