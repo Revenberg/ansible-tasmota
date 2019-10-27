a5115bf2a8edc077c571cc66021cc40cd9bab050
Your Name <you@example.com>
2019-10-27 19:15:45 +0100

    .

--- roles/tasmota/files/wifi.sh
+++ roles/tasmota/files/wifi.sh
@@ -6,10 +6,10 @@ while [ 1 ]; do
 
   ESSID=$(iwlist wlan0 scan | grep ITEAD | cut -d'"' -f 2)
   if [ "$ESSID" != "" ]; then
-    echo $ESSID
+    echo $ESSID > $4
     if [ -f "~/ESSID_$ESSID.txt" ]; then
-      sudo nmcli device wifi con "$ESSID" password "12345678"
-      "python3 /home/pi/SonOTA-master/SonOTA.py --serving-host $1 --wifi-ssid $2 --wifi-password $3 1>~/SonOTA.log"
+      sudo nmcli device wifi con "$ESSID" password "12345678" > $4
+      "python3 /home/pi/SonOTA-master/SonOTA.py --serving-host $1 --wifi-ssid $2 --wifi-password $3 > $4"
       rm ~/ESSID_*.txt  2>/dev/null
       date > ~/ESSID_$ESSID.txt
     fi
@@ -17,14 +17,14 @@ while [ 1 ]; do
 
   ESSID=$(iwlist wlan0 scan | grep FinalStage | cut -d'"' -f 2)
   if [ "$ESSID" == "FinalStage" ]; then
-    echo $ESSID
+    echo $ESSID > $4
     if [ -f "~/ESSID_FinalStage.txt" ]; then
-      sudo nmcli device wifi con "FinalStage"
+      sudo nmcli device wifi con "FinalStage" > $4
       rm ~/ESSID_*.txt 2>/dev/null
       date > ~/ESSID_FinalStage.txt
     fi
   fi
   sleep 1
 
-  date
+  date > $4
 done
